703e873dc3385cb31977249f00ec48da5fe8b952
Alan <alan@A-CM-MBP.local>
2020-12-13 20:03:37 -0800

    More work in progress.
    Decided to get rid of the rotation and make my own board grid
    with a local array that will indicate what to display.  About
    to re-work all the drawing logic, but it might be good to keep
    some of this around in the event I need to see what I was thinking
    of before.

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,6 @@ use ggez::timer;
 use ggez::{Context, GameResult};
 use ggez::input;
 
-struct MainState {
-    meshes: Vec<graphics::Mesh>,
-    rotation: f32,
-    piece_x: f32,
-    piece_y: f32,
-    board: [[bool; 10]; 20],
-}
-
 enum Tetrominoes {
     I,
     O,
@@ -26,16 +18,28 @@ enum Tetrominoes {
     Z,
 }
 
+const BOARD_HEIGHT: usize = 20;
+const BOARD_WIDTH: usize = 10;
+
+struct MainState {
+    meshes: Vec<graphics::Mesh>,
+    rotation: u8,
+    piece_type: Tetrominoes,
+    piece_x: usize,
+    piece_y: usize,
+    board: [[bool; BOARD_HEIGHT]; BOARD_WIDTH],
+}
+
 impl MainState {
-    // I think this should be the static background that never changes
     fn new(ctx: &mut Context) -> GameResult<MainState> {
         let meshes = vec![build_mesh(ctx)?];
         let s = MainState {
             meshes,
-            rotation: 0.0,
-            piece_x: 300.0,
-            piece_y: 40.0,
-            board: [[true; 10]; 20],
+            rotation: 0,
+            piece_type: Tetrominoes::I,
+            piece_x: 2,
+            piece_y: 4,
+            board: [[false; BOARD_HEIGHT]; BOARD_WIDTH],
         };
         Ok(s)
     }
@@ -44,12 +48,13 @@ impl MainState {
 fn build_mesh(ctx: &mut Context) -> GameResult<graphics::Mesh> {
     let mb = &mut graphics::MeshBuilder::new();
 
+    // This is the game border
     mb.line(
         &[
-            Point2::new(420.0, 20.0),
-            Point2::new(420.0, 420.0),
-            Point2::new(200.0, 420.0),
-            Point2::new(200.0, 20.0),
+            Point2::new(400.0, 0.0),
+            Point2::new(400.0, 410.0),
+            Point2::new(180.0, 410.0),
+            Point2::new(180.0, 0.0),
         ],
         20.0,
         Color::new(0.5, 0.5, 1.0, 4.0),
@@ -93,25 +98,29 @@ impl EventHandler for MainState {
 		input::keyboard::KeyCode::Q => quit(ctx),
 		input::keyboard::KeyCode::W => {
                     // Check for rotation being legit
-		    self.rotation += 90.0f32;
-		    if self.rotation >= 360.0 {
-			self.rotation = 0.0;
+		    self.rotation += 1;
+		    if self.rotation > 3 {
+			self.rotation = 0;
 		    }
 		}
 		input::keyboard::KeyCode::A => {
-                    // Check for -10x 
-		    self.piece_x -= 20.0f32;
-		    if self.piece_x <= 200.0 {
-			self.piece_x = 200.0;
+                    if self.piece_x > 0 {
+                        self.piece_x -= 1;
 		    }
 		}
 		input::keyboard::KeyCode::D => {
-                    // Check for +10x 
-		    self.piece_x += 20.0f32;
-		    if self.piece_x >= 400.0 {
-			self.piece_x = 400.0;
+		    if self.piece_x < BOARD_WIDTH - 1 {
+			self.piece_x += 1;
 		    }
 		}
+		input::keyboard::KeyCode::S => {
+		    if self.piece_y < BOARD_HEIGHT - 1 {
+			self.piece_y += 1;
+		    }
+		}
+		input::keyboard::KeyCode::Z => {
+                    println!("{:#?}", self.board);
+                }
 		_ => (),
 	    }
 	}
@@ -120,45 +129,91 @@ impl EventHandler for MainState {
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
 
-        // Text
-        let text = graphics::Text::new(format!("Rotation:{}", self.rotation));
-        graphics::draw(ctx, &text,
-                       (Point2::new(20.0, 20.0),
-                       graphics::WHITE),)?;
-
-        // Create and draw a filled rectangle mesh.
-        let rect = graphics::Rect::new(0.0, 0.0, 60.0, 20.0);
-        let r1 = graphics::Mesh::new_rectangle(ctx,
-                                               DrawMode::fill(),
-                                               rect,
-                                               graphics::WHITE)?;
-
-        // This is where the object will be placed
-        let dst2 = cgmath::Point2::new(self.piece_x, self.piece_y);
-        // ZZZ scale allows us to scale the image
-        let scale = cgmath::Vector2::new(1.0, 1.0);
-        graphics::draw(ctx, &r1, graphics::DrawParam::new()
-                                    .dest(dst2)
-                                    .rotation(self.rotation.to_radians())
-                                    .offset(Point2::new(30.0, 0.0))
-                                    .scale(scale),
-                        )?;
-
-
-        // Create and draw a stroked rectangle mesh.
-        let rect = graphics::Rect::new(450.0, 450.0, 50.0, 50.0);
-        let r2 = graphics::Mesh::new_rectangle(
-            ctx,
-            DrawMode::stroke(2.0),
-            rect,
-            graphics::Color::new(1.0, 0.0, 0.0, 1.0),
-        )?;
-        graphics::draw(ctx, &r2, DrawParam::default())?;
-
         // Draw some pre-made meshes
         for m in &self.meshes {
             graphics::draw(ctx, m, DrawParam::new())?;
         }
+
+        // Text
+        let text = graphics::Text::new(format!("Rotation:{}", self.rotation));
+        graphics::draw(ctx, &text,
+                       (Point2::new(20.0, 20.0), graphics::WHITE),)?;
+        let text = graphics::Text::new(format!("piece_x:{}", self.piece_x));
+        graphics::draw(ctx, &text,
+                       (Point2::new(20.0, 40.0), graphics::WHITE),)?;
+        let text = graphics::Text::new(format!("piece_y:{}", self.piece_y));
+        graphics::draw(ctx, &text,
+                       (Point2::new(20.0, 60.0), graphics::WHITE),)?;
+
+        // This is the game grid
+        // The x and y index are manually computed each time
+        // we walk through the loop.  This is not a good way to do
+        // this, we should come up with something better.  Maybe just
+        // do all the work compute array ahead of time, then only
+        // display
+
+        let mb = &mut graphics::MeshBuilder::new();
+        let mut py = 0;
+        for y in (0..400).step_by(20) {
+            let mut px = 0;
+            for x in (200..400).step_by(20) {
+                let x = x as f32;
+                let y = y as f32;
+                mb.line(
+                    &[
+                        Point2::new(x, y),
+                        Point2::new(x, y + 20.0),
+                    ],
+                    20.0,
+                    Color::new(0.9, 0.9, 0.9, 4.0),
+                )?;
+
+                if self.board[px][py] == false {
+                    let fill = Color::new(0.0, 0.0, 0.0, 4.0);
+                    mb.line(
+                        &[
+                            Point2::new(x, y + 1.0),
+                            Point2::new(x, y + 19.0),
+                        ],
+                        18.0,
+                        fill,
+                    )?;
+                }
+                px += 1;
+            }
+            py += 1;
+        }
+
+        // Figure out where our piece is
+        /*
+        match self.piece_type {
+            Tetrominoes::I => {
+                if self.rotation == 0 { // ----
+                }
+                else if self.rotation == 1 { //  |
+                    self.board[self.p_x + 2][self.p_y] = true;
+                    self.board[self.p_x + 2][self.p_y + 1] = true;
+                    self.board[self.p_x + 2][self.p_y + 2] = true;
+                    self.board[self.p_x + 2][self.p_y + 3] = true;
+                }
+                else if self.rotation == 2 { // ____
+                    self.board[self.p_x][self.p_y + 2] = true;
+                    self.board[self.p_x + 1][self.p_y + 2] = true;
+                    self.board[self.p_x + 2][self.p_y + 2] = true;
+                    self.board[self.p_x + 3][self.p_y + 2] = true;
+                }
+                else  /* self.rotation == 3 */ { // |
+                    self.board[self.p_x + 1][self.p_y] = true;
+                    self.board[self.p_x + 1][self.p_y + 1] = true;
+                    self.board[self.p_x + 1][self.p_y + 2] = true;
+                    self.board[self.p_x + 1][self.p_y + 3] = true;
+                }
+            },
+            _ => (),
+        }
+        */
+        let m = mb.build(ctx)?;
+        graphics::draw(ctx, &m, DrawParam::new())?;
 
         // Finished drawing, show it all on the screen!
         graphics::present(ctx)?;
